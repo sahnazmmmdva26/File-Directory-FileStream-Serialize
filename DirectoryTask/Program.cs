db2c40a2c1d8f0535efae7b72029b8cc4d67b187
sahnazmmmdva <84397620+sahnazmmmdva@users.noreply.github.com>
2022-12-09 01:31:36 +0400

    added new file

--- DirectoryTask/Program.cs
+++ DirectoryTask/Program.cs
@@ -4,14 +4,19 @@ namespace DirectoryTask
 {
     internal class Program
     {
-        static string path = @"C:\Users\HP\Desktop\c# files\DirectoryTask\DirectoryTask\DirectoryTask.csproj";
+        static string path = @"C:\Users\HP\Desktop\c# files\DirectoryTask\DirectoryTask";
       
         static void Main(string[] args) 
         {
             List<string> list = new List<string>();
             list.Add("Shahnaz");
             list.Add("Ulviyya");
+            Helper.SaveAsJson(list, Path.Combine(path, "name.json"));
+            Add("Ehtiram");
             Search("Ulviyya");
+            Update(1, "Fateh");
+            Delete(0);
+            
         }
         static void Add(string name)
         {
@@ -19,8 +24,8 @@ namespace DirectoryTask
             if (!namessFromJson.Contains(name))
             {
                 namessFromJson.Add(name);
-            }
             Helper.SaveAsJson(namessFromJson, Path.Combine(path,"name.json"));
+            }
         }
         static bool Search(string name)
         {
@@ -41,12 +46,12 @@ namespace DirectoryTask
             if (namessFromJson.Count > index && index>=0)
             {
                 namessFromJson[index] = name;
+            Helper.SaveAsJson(namessFromJson, Path.Combine(path, "name.json"));
             }
             else
             {
                 Console.WriteLine("Indexi duzgun daxil edin");
             }
-            Helper.SaveAsJson(namessFromJson, Path.Combine(path, "name.json"));
         }
         static void Delete(int index)
         {
@@ -54,12 +59,12 @@ namespace DirectoryTask
             if (namessFromJson.Count > index && index >= 0)
             {
                 namessFromJson.Remove(namessFromJson[index]);
+            Helper.SaveAsJson(namessFromJson, Path.Combine(path, "name.json"));
             }
             else
             {
                 Console.WriteLine("Indexi duzgun daxil edin");
             }
-            Helper.SaveAsJson(namessFromJson, Path.Combine(path, "name.json"));
         }
     }
 }
